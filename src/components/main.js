54c6101656af4ef24e5b9bd294bb048f091b30fa
PaulMac66 <paul@polarisdesign.co.uk>
2017-06-02 14:19:22 +0100

    setup finished

---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main">
+          <div className='Container'>
+            <div id='Main' >Main section go here...............</div>
+            
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
